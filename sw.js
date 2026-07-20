921e503be49ab3e63bb6929b3c66eafb59cc88cf
sifat48139929 <sifat48139929@gmail.com>
2026-07-20 23:20:25 +0600

    Update zoneId in service worker configuration

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11357972
+    "zoneId": 11358301
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
